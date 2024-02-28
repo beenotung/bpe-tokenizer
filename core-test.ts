80fe8a6777801188425b7d1989a3a2e559cffa73
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-02-28 17:03:30 +0800

    feat: compact the vector index by skipping tokens without weights after merge docs: add jsdoc to explain the purpose of properties

--- core-test.ts
+++ core-test.ts
@@ -15,12 +15,17 @@ if (!fs.existsSync(config_file)) {
   // you can set a higher threshold for the minimum number of occurrences
   tokenizer.mergeUntil({ min_weight: 2 })
 
+  // to skip index on tokens without weights after merge
+  tokenizer.compactVectorIndex()
+
   // save the tokenizer model to file
   writeFileSync(config_file, JSON.stringify(tokenizer))
 } else {
   // load the tokenizer model from file
   tokenizer.fromJSON(JSON.parse(fs.readFileSync(config_file).toString()))
 }
+
+writeFileSync('tokens.json', JSON.stringify(tokenizer.token_table))
 
 // encode into object array for extended usage
 let tokens = tokenizer.encodeToTokens(content)
@@ -29,8 +34,12 @@ let tokens = tokenizer.encodeToTokens(content)
 let vector = tokenizer.encodeToVector(content)
 
 // you can decode from token or vector
-let decoded_content = tokenizer.decodeTokens(tokens)
-decoded_content = tokenizer.decodeVector(vector)
+let decoded_content_from_tokens = tokenizer.decodeTokens(tokens)
+let decoded_content_from_vector = tokenizer.decodeVector(vector)
+
+let used_tokens = tokenizer.token_table.filter(
+  token => token.vector_index != null,
+)
 
 console.log({
   // content,
@@ -38,8 +47,38 @@ console.log({
   content: content.length,
   vector: vector.length,
   ratio: vector.length / content.length,
+  all_tokens: tokenizer.token_table.length,
+  used_tokens: used_tokens.length,
   // tokens,
-  segments: tokens.map(token => token.chars),
+  // segments: tokens.map(token => token.chars),
   // decoded_content,
-  match: content == decoded_content,
+  match:
+    content == decoded_content_from_tokens &&
+    decoded_content_from_tokens == decoded_content_from_vector,
+  token_char_length: used_tokens
+    .slice()
+    .reduce((acc, c) => {
+      let item = acc.find(({ chars }) => chars == c.chars.length)
+      if (item) {
+        item.count++
+      } else {
+        acc.push({ chars: c.chars.length, count: 1 })
+      }
+      return acc
+    }, [] as { chars: number; count: number }[])
+    .sort((a, b) => a.chars - b.chars),
+  weights: used_tokens
+    .slice()
+    .reduce((acc, c) => {
+      let item = acc.find(({ weight }) => weight == c.weight)
+      if (item) {
+        item.count++
+      } else {
+        acc.push({ weight: c.weight, count: 1 })
+      }
+      return acc
+    }, [] as { weight: number; count: number }[])
+    .sort((a, b) => a.weight - b.weight),
+  min: Math.min(...vector),
+  max: Math.max(...vector),
 })
--- core.ts
+++ core.ts
@@ -1,8 +1,14 @@
 export type Token = {
   chars: string
+  /** @description the weight after merge */
   weight: number
+  /** @description the weight before merge */
+  original_weight: number
   code: string
+  /** @description including zero-weight tokens */
   index: number
+  /** @description excluding zero-weight tokens */
+  vector_index: number | null
 }
 
 /**
@@ -18,37 +24,100 @@ export type MergeCode = [a: string, b: string]
 export let EOF = String.fromCharCode(4)
 
 export class BPETokenizer {
+  /** @description index for lookup */
   char_to_token: Record<string, Token> = {}
+
+  /** @description index for lookup */
   code_to_token: Record<string, Token> = {}
 
+  /** @description token.index -> Token */
   token_table: Token[] = []
 
+  /** @description token.vector_index -> Token */
+  vector_index_to_token: Token[] = []
+
+  /** @description for export */
+  merge_tokens: MergeToken[] = []
+
+  /** @description for encode */
   merge_codes: MergeCode[] = []
 
+  /** @description added by this.addContent() */
   corpus_in_code: string[] = []
 
   toJSON() {
+    let { merge_tokens } = this
+    let token_table: [chars: string, weight: number][] = []
+    token_loop: for (let token of this.token_table) {
+      let char_count = 0
+      for (let _char of token.chars) {
+        char_count++
+        if (char_count > 1) break token_loop
+      }
+      token_table.push([token.chars, token.original_weight])
+    }
     return {
-      token_table: this.token_table,
-      merge_codes: this.merge_codes,
+      version: 1,
+      token_table,
+      merge_codes: merge_tokens.map(
+        ([a, b, c]) => [a.code, b.code, c.original_weight] as const,
+      ),
     }
   }
 
   fromJSON(json: ReturnType<(typeof this)['toJSON']>) {
+    if (
+      json.version !== 1 ||
+      !Array.isArray(json.token_table) ||
+      !Array.isArray(json.merge_codes)
+    )
+      throw new Error('invalid format')
     let newInstance = new BPETokenizer()
-    let { char_to_token, code_to_token, token_table } = newInstance
-    newInstance.merge_codes = json.merge_codes
+    let {
+      char_to_token,
+      code_to_token,
+      token_table,
+      merge_tokens,
+      merge_codes,
+    } = newInstance
     Object.assign(this, newInstance)
-    for (let token of json.token_table) {
-      let char_count = 0
-      for (let char of token.chars) {
-        char_count++
+    for (let [char, weight] of json.token_table) {
+      let index = token_table.length
+      let code = String.fromCodePoint(index)
+      let token: Token = {
+        chars: char,
+        weight,
+        original_weight: weight,
+        code,
+        index,
+        vector_index: null,
       }
-      if (char_count == 1) {
-        char_to_token[token.chars] = token
+      char_to_token[char] = token
+      code_to_token[code] = token
+      token_table[index] = token
+    }
+    for (let [a_code, b_code, c_weight] of json.merge_codes) {
+      let a: Token = code_to_token[a_code]
+      let b: Token = code_to_token[b_code]
+      let new_index = token_table.length
+      let new_code = String.fromCodePoint(new_index)
+      let c: Token = {
+        chars: a.chars + b.chars,
+        weight: c_weight,
+        original_weight: c_weight,
+        code: new_code,
+        index: new_index,
+        vector_index: null,
       }
-      code_to_token[token.code] = token
-      token_table[token.index] = token
+      a.weight -= c_weight
+      if (a != b) {
+        b.weight -= c_weight
+      }
+      char_to_token[c.chars] = c
+      code_to_token[c.code] = c
+      token_table[c.index] = c
+      merge_tokens.push([a, b, c])
+      merge_codes.push([a.code + b.code, c.code])
     }
   }
 
@@ -60,16 +129,37 @@ export class BPETokenizer {
       if (!token) {
         let index = token_table.length
         let code = String.fromCodePoint(index)
-        token = { chars: char, weight: 1, code: code, index }
+        token = {
+          chars: char,
+          weight: 1,
+          original_weight: 1,
+          code: code,
+          index,
+          vector_index: null,
+        }
         char_to_token[char] = token
         code_to_token[code] = token
         token_table.push(token)
       } else {
         token.weight++
+        token.original_weight++
       }
       sample_in_code += token.code
     }
     this.corpus_in_code.push(sample_in_code)
+  }
+
+  compactVectorIndex() {
+    let { vector_index_to_token } = this
+    let vector_index = 0
+    vector_index_to_token.length = 0
+    for (let token of this.token_table) {
+      if (token.weight > 0) {
+        token.vector_index = vector_index
+        vector_index_to_token[vector_index] = token
+        vector_index++
+      }
+    }
   }
 
   findNextMerge(): MergeToken | null {
@@ -99,8 +189,10 @@ export class BPETokenizer {
             c = {
               chars: a.chars + b.chars,
               weight: 1,
+              original_weight: 1,
               code: new_code,
               index: new_index,
+              vector_index: null,
             }
             b_c_tokens.set(b, c)
           } else {
@@ -117,11 +209,21 @@ export class BPETokenizer {
       }
     }
 
-    return max_c ? [max_a!, max_b!, max_c] : null
+    if (!max_c) return null
+
+    max_c.original_weight = max_c.weight
+
+    return [max_a!, max_b!, max_c]
   }
 
   applyMerge(merge: MergeToken) {
-    let { code_to_token, token_table, merge_codes, corpus_in_code } = this
+    let {
+      code_to_token,
+      token_table,
+      merge_tokens,
+      merge_codes,
+      corpus_in_code,
+    } = this
     let [a, b, c] = merge
 
     let from_code = a.code + b.code
@@ -135,6 +237,7 @@ export class BPETokenizer {
     code_to_token[c.code] = c
     token_table.push(c)
 
+    merge_tokens.push(merge)
     merge_codes.push([from_code, to_code])
 
     let corpus_count = corpus_in_code.length
@@ -148,7 +251,7 @@ export class BPETokenizer {
     for (;;) {
       let merge = this.findNextMerge()
       if (!merge) break
-      let [a, b, c] = merge
+      let [_a, _b, c] = merge
       if (c.weight < min_weight) break
       this.applyMerge(merge)
     }
@@ -159,7 +262,11 @@ export class BPETokenizer {
 
     let content_in_code = ''
     for (let char of content) {
-      content_in_code += char_to_token[char].code
+      let token = char_to_token[char]
+      if (!token) {
+        throw new Error('unknown token, char: ' + JSON.stringify(char))
+      }
+      content_in_code += token.code
     }
 
     for (let [from_code, to_code] of this.merge_codes) {
@@ -189,7 +296,17 @@ export class BPETokenizer {
 
     let vector: number[] = []
     for (let code of content_in_code) {
-      vector.push(code_to_token[code].index)
+      let { vector_index } = code_to_token[code]
+      if (!vector_index) {
+        this.compactVectorIndex()
+        vector_index = code_to_token[code].vector_index
+        if (!vector_index) {
+          throw new Error(
+            'compactVectorIndex() not called before encoding or saving',
+          )
+        }
+      }
+      vector.push(vector_index)
     }
 
     return vector
@@ -204,10 +321,10 @@ export class BPETokenizer {
   }
 
   decodeVector(vector: number[]): string {
-    let { token_table } = this
+    let { vector_index_to_token } = this
     let content = ''
     for (let index of vector) {
-      content += token_table[index].chars
+      content += vector_index_to_token[index].chars
     }
     return content
   }
