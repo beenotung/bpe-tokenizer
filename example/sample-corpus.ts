d3d2cc873cd46e86de656e5b253c209856bd7797
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-03-27 16:28:19 +0800

    example: update sample-corpus to split dirname and filename

--- example/sample-corpus.ts
+++ example/sample-corpus.ts
@@ -3,10 +3,22 @@ import { createReadStream, existsSync } from 'fs'
 import { stream_lines } from '@beenotung/tslib/file-stream'
 
 let file = 'data.txt'
-let maxLines = 100000
+let maxLines = 10000
 
 if (!existsSync(file)) {
-  execSync(`find $HOME | grep -v .git | head -n ${maxLines} > ${file}`)
+  // skip hash-like filenames
+  execSync(`find $HOME -type f \
+  | grep -v .git \
+  | grep -v .cache \
+  | grep -v .dartServer \
+  | grep -v .zoom \
+  | grep -v .gnome \
+  | grep -v .keras \
+  | grep -v .ipfs \
+  | grep -v .mozilla \
+  | grep -v postgresql \
+  | grep -v history \
+  | head -n ${maxLines} > ${file}`)
 }
 
 export function count_corpus(): number {
@@ -23,8 +35,14 @@ export function count_corpus(): number {
 export async function* load_corpus_list() {
   let stream = createReadStream(file)
   let id = 0
-  for await (let content of stream_lines(stream)) {
+  for await (let file of stream_lines(stream)) {
+    let parts = file.split('/')
+    let filename = parts.pop()
+    for (let dirname of parts) {
+      id++
+      yield { id, content: dirname + '/' }
+    }
     id++
-    yield { id, content }
+    yield { id, content: filename + '\n' }
   }
 }
